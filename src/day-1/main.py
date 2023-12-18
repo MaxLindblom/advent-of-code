b6f623f68b0efa05374e50f7079a90de0a78745e
Max Lindblom <max.g.lindblom@gmail.com>
2023-12-18 16:22:10 +0100

    fix formatting with black

--- src/day-1/main.py
+++ src/day-1/main.py
@@ -8,28 +8,29 @@ from src.files.read import file_to_array
 from src.print_results import print_results
 
 CALIBRATIONS = {
-    'one': '1',
-    'two': '2',
-    'three': '3',
-    'four': '4',
-    'five': '5',
-    'six': '6',
-    'seven': '7',
-    'eight': '8',
-    'nine': '9',
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
 }
 
 CALIBRATIONS_REVERSED = {
-    'eno': '1',
-    'owt': '2',
-    'eerht': '3',
-    'ruof': '4',
-    'evif': '5',
-    'xis': '6',
-    'neves': '7',
-    'thgie': '8',
-    'enin': '9',
+    "eno": "1",
+    "owt": "2",
+    "eerht": "3",
+    "ruof": "4",
+    "evif": "5",
+    "xis": "6",
+    "neves": "7",
+    "thgie": "8",
+    "enin": "9",
 }
+
 
 def get_calibration_value_part_one(sequence: str) -> int:
     """
@@ -40,13 +41,14 @@ def get_calibration_value_part_one(sequence: str) -> int:
     first = sequence[bools.index(True)]
     last = sequence[len(bools) - 1 - bools[::-1].index(True)]
 
-    return int(first+last)
+    return int(first + last)
+
 
 def parse_calibration_sequence(sequence: str, calibration_map: dict) -> str:
     """
     Parses a calibration sequence
     """
-    word = ''
+    word = ""
     for char in sequence:
         for key, value in calibration_map.items():
             if word.find(key) != -1:
@@ -55,7 +57,8 @@ def parse_calibration_sequence(sequence: str, calibration_map: dict) -> str:
             return char
         word += char
 
-    return '0'
+    return "0"
+
 
 def get_calibration_value_part_two(sequence: str) -> int:
     """
@@ -64,18 +67,24 @@ def get_calibration_value_part_two(sequence: str) -> int:
     first = parse_calibration_sequence(sequence, CALIBRATIONS)
     last = parse_calibration_sequence(sequence[::-1], CALIBRATIONS_REVERSED)
 
-    return int(first+last)
+    return int(first + last)
+
 
 def main():
     """
     Runs the main script
     """
-    sequences = file_to_array(Path(__file__).with_name('input.txt'))
+    sequences = file_to_array(Path(__file__).with_name("input.txt"))
 
-    first_result = reduce(lambda x, y: x+get_calibration_value_part_one(y), sequences, 0)
-    second_result = reduce(lambda x, y: x+get_calibration_value_part_two(y), sequences, 0)
+    first_result = reduce(
+        lambda x, y: x + get_calibration_value_part_one(y), sequences, 0
+    )
+    second_result = reduce(
+        lambda x, y: x + get_calibration_value_part_two(y), sequences, 0
+    )
 
     print_results(first_result, second_result)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- src/day-2/main.py
+++ src/day-2/main.py
@@ -7,69 +7,71 @@ from pathlib import Path
 from src.files.read import file_to_array
 from src.print_results import print_results
 
+
 def parse_game(game: str) -> dict:
     """
     Parses a game, returning the maxes for each color
     While regex is probably more appropriate, this should work fine
     """
-    (game, rounds) = game.split(':')
-    game_id = int(game.split(' ')[1])
-    sets = rounds.split(';')
+    (game, rounds) = game.split(":")
+    game_id = int(game.split(" ")[1])
+    sets = rounds.split(";")
 
     max_red = max_green = max_blue = 0
 
     for s in sets:
-        number_colors = s.split(',')
+        number_colors = s.split(",")
         for e in number_colors:
-            [value, color] = e.strip().split(' ')
-            if color == 'red':
+            [value, color] = e.strip().split(" ")
+            if color == "red":
                 max_red = max(max_red, int(value))
-            elif color == 'green':
+            elif color == "green":
                 max_green = max(max_green, int(value))
-            elif color == 'blue':
+            elif color == "blue":
                 max_blue = max(max_blue, int(value))
 
     return {
-        'id': game_id,
-        'max_red': max_red,
-        'max_green': max_green,
-        'max_blue': max_blue
+        "id": game_id,
+        "max_red": max_red,
+        "max_green": max_green,
+        "max_blue": max_blue,
     }
+
 
 def get_id_if_valid_game(parsed_game: dict, bag_config: dict) -> int:
     """
     Returns the id of the game if it is valid, otherwise returns 0
     """
     if (
-        parsed_game['max_red'] > bag_config['red'] or
-        parsed_game['max_green'] > bag_config['green'] or
-        parsed_game['max_blue'] > bag_config['blue']
+        parsed_game["max_red"] > bag_config["red"]
+        or parsed_game["max_green"] > bag_config["green"]
+        or parsed_game["max_blue"] > bag_config["blue"]
     ):
         return 0
-    return parsed_game['id']
+    return parsed_game["id"]
+
 
 def get_power_of_game(parsed_game: dict) -> int:
     """
     Returns the power of the game
     """
-    return parsed_game['max_red'] * parsed_game['max_green'] * parsed_game['max_blue']
+    return parsed_game["max_red"] * parsed_game["max_green"] * parsed_game["max_blue"]
+
 
 def main():
     """
     Runs the main script
     """
-    games = file_to_array(Path(__file__).with_name('input.txt'))
-    bag_config = {'red': 12, 'green': 13, 'blue': 14}
+    games = file_to_array(Path(__file__).with_name("input.txt"))
+    bag_config = {"red": 12, "green": 13, "blue": 14}
 
     first_result = reduce(
-        lambda x,
-        y: x+get_id_if_valid_game(parse_game(y), bag_config),
-        games,
-        0
+        lambda x, y: x + get_id_if_valid_game(parse_game(y), bag_config), games, 0
     )
-    last_result = reduce(lambda x, y: x+get_power_of_game(parse_game(y)), games, 0)
+    last_result = reduce(lambda x, y: x + get_power_of_game(parse_game(y)), games, 0)
 
     print_results(first_result, last_result)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
