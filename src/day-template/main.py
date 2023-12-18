846e9cba7f48e649ed3c93a7bffb05be6e25969a
Max Lindblom <max.g.lindblom@gmail.com>
2023-12-18 15:18:04 +0100

    add problem template

---
+++ src/day-template/main.py
@@ -0,0 +1,21 @@
+""""
+Main script for the day X problem
+"""
+
+from pathlib import Path
+from src.files.read import file_to_array
+from src.print_results import print_results
+
+def main():
+    """
+    Runs the main script
+    """
+    sequences = file_to_array(Path(__file__).with_name('input.txt'))
+
+    first_result = 0
+    second_result = 0
+
+    print_results(first_result, second_result)
+
+if __name__ == '__main__':
+    main()
